a40686e860b06b6e4d2122b8327cf6be4009d9fa
root <root@WIN-UEN79IPA7NU>
2024-06-04 16:10:07 +0300

    Some fixed build scripts

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -2,8 +2,14 @@
 
 #sudo apt-get install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
 
+if [ $# -eq 0 ];then
+    th="-j1"
+else
+    th="$@"
+fi
+
 git submodule update --init riscv-gnu-toolchain
-mkdir toolchain_bin
+mkdir -p toolchain
 cd riscv-gnu-toolchain
 git submodule update --init --recursive binutils gcc gdb
 cd binutils
@@ -14,6 +20,6 @@ cd ../gdb
 git apply ../gdb.patch
 cd ..
 pwd_path=`pwd`
-./configure --prefix=${pwd_path}/../toolchain_bin/ --disable-linux --with-arch=rv64gcv
-make newlib -j
+./configure --prefix=${pwd_path}/../toolchain/ --disable-linux --with-arch=rv64gcv
+make newlib $th
 cd ..
